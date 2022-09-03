e79a42fe500958d86cfc2272513259bf63ed5f56
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-09-03 17:04:26 -0500

    untested crypto

---
+++ packages/typescript/src/crypto.ts
@@ -0,0 +1,76 @@
+/* eslint-disable max-classes-per-file */
+import { subtle, getRandomValues } from 'crypto';
+
+class crypto {
+	public static async generateKey() {
+		return subtle.generateKey(
+			{
+				name: 'AES-GCM',
+				length: 128,
+			},
+			true,
+			['encrypt', 'decrypt'],
+		);
+	}
+
+	public static async exportKey(key: CryptoKey) {
+		return subtle.exportKey('raw', key);
+	}
+
+	public static async importKey(raw: ArrayBuffer) {
+		return subtle.importKey(
+			'raw',
+			raw,
+			{
+				name: 'AES-GCM',
+			},
+			false,
+			['encrypt', 'decrypt'],
+		);
+	}
+
+	public static async encrypt(str: string, key: CryptoKey) {
+		const encoded = new TextEncoder().encode(str);
+		const iv = getRandomValues(new Uint8Array(12));
+		const encrypted = await subtle.encrypt(
+			{ name: 'AES-GCM', iv } as AesGcmParams,
+			key,
+			encoded,
+		);
+
+		const buf = Buffer.from(encrypted);
+
+		return Buffer.concat([iv, buf]);
+	}
+
+	public static async decrypt(encrypted: ArrayBuffer, key: ArrayBuffer) {
+		const thekey = await crypto.importKey(key);
+		const decrypted = await subtle.decrypt({ name: 'AES-GCM' }, thekey, encrypted);
+		const decoded = new TextDecoder().decode(decrypted);
+		return decoded;
+	}
+}
+
+export class ClientSideCrypto<T = object> {
+	public async encrypt(obj: T) {
+		const key = await crypto.generateKey();
+
+		const raw = await crypto.encrypt(JSON.stringify(obj), key);
+
+		const exported = await crypto.exportKey(key);
+
+		return {
+			ciphertext: Buffer.from(raw).toString('base64'),
+			key: Buffer.from(exported).toString('base64'),
+		};
+	}
+
+	public async decrypt(ciphertext64: string, key64: string) {
+		const cyphertext = Buffer.from(ciphertext64, 'base64');
+
+		const key = Buffer.from(key64, 'base64');
+		const dec = await crypto.decrypt(cyphertext, key);
+
+		return JSON.parse(dec) as T;
+	}
+}
